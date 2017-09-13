89fa280d087407af6a21ebe42b71aa4e6f7b42a3
James Reidy <jreidy@cornell.edu>
2017-09-13 17:55:13 -0400

    correct the example comment

--- timing_ab.sh
+++ timing_ab.sh
@@ -3,10 +3,9 @@
 # Connection Times (ms)
 #  min  mean[+/-sd] median   max timestamp site
 #  blank delimited
-# Example:
-# 1951 2334 508.1 2180 3226 2017-09-13.14:49:04 newcatalog.library.cornell.edu/
-# 1081 1557 505.9 1670 2174 2017-09-13.14:49:04 newcatalog.library.cornell.edu/catalog?utf8=%E2%9C%93&q=book+of+knots&search_field=all_fields
-# 277 289 10.6 293 303 2017-09-13.14:49:04 newcatalog.library.cornell.edu/catalog/6885529
+# Example - .csv format:
+# min,mean,standard_deviation,median,max,timestamp,site
+# 1276,1811,498.8,2079,2263,2017-09-13.17:47:04,newcatalog.library.cornell.edu/
 
 targets=(
   "newcatalog.library.cornell.edu/"
